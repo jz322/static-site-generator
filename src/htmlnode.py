af148ac6ebca581abd2e3f4e55535a19378fe185
jz322 <joshuazimmerman33@gmail.com>
2026-01-14 01:35:35 +0000

    LeafNode support

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,3 @@
-
-
 class HTMLNode():
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -22,3 +20,28 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        string = ""
+        if self.value == None:
+            raise ValueError
+        elif self.tag == None:
+            string = str(self.value)
+            return string
+        
+        if self.props is None or self.props == {}:
+            string = f"<{self.tag}>{self.value}</{self.tag}>"
+        else:
+            html_prop = self.props_to_html()
+            string = f'<{self.tag}{html_prop}>{self.value}</{self.tag}>'
+
+        return string 
+
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
